8919910e06fab93ba703cfcafdd3d83da2320f98
Muttita <muttita.c@kkumail.com>
2024-09-18 18:18:42 +0700

    ีupdate

--- exploreData.py
+++ exploreData.py
@@ -1,15 +1,4 @@
-# Let's first inspect the file to see what kind of data it contains.
-file_path = 'C:/Users/ROG/Downloads/products_dict.p'
-
-# Since the file has a .p extension, it might be a Python pickle file.
-# Let's try loading it to check its contents.
-import pickle
-import json
-
-# Attempt to load the pickle file and check its content
-with open(file_path, 'rb') as file:
-    data = pickle.load(file)
-
-# แสดงผล JSON ในรูปแบบที่สวยงาม
-pretty_json = json.dumps(data, indent=4)
-print(pretty_json)
+import pandas as pd
+df = pd.read_json("json\product\product-in-subImg\product-in-subImg-data.json")
+print(len(df))
+print(df.duplicated().sum())
